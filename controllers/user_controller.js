18fedb54378566dfc35a5cf5d415434dc4440dca
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-05-21 01:48:03 +0900

    user route, controller 분리

---
+++ controllers/user_controller.js
@@ -0,0 +1,37 @@
+var { sequelize, Sequelize } = require("../models/index");
+const Users = require("../models/users")(sequelize, Sequelize);
+
+// 사용자 전체 조회 DESC 정렬
+exports.getAllUsers = async (req, res) => {
+    await Users.findAll({ order: [["id", "DESC"]] })
+      .then((result) => {
+        res
+          .status(200)
+          .json({ code: 200, message: "사용자 목록조회 성공", data: { users: result } });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+};
+
+// get one user
+exports.getUser = async (req, res) => {
+  await Users.findOne({
+    where: { id: req.params.id },
+  })
+    .then((result) => {
+      console.log(result);
+      if (result) {
+        res
+          .status(200)
+          .json({ code: 200, message: "사용자 조회 성공", data: { user: result } });
+      } else {
+        res
+          .status(404)
+          .json({ code: 404, message: "사용자가 존재하지 않습니다." });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- routes/user_router.js
+++ routes/user_router.js
@@ -1,40 +1,120 @@
 const express = require("express");
 const router = express.Router();
-var { sequelize, Sequelize } = require("../models/index");
-const Users = require("../models/users")(sequelize, Sequelize);
 
+const token = require("../middleware/jwt");
+
+const user_controller = require("../controllers/user_controller");
+
+/**
+ * @swagger
+ * /user:
+ *    get:
+ *      summary: 사용자 목록조회
+ *      description: 전체 사용자 목록 조회
+ *      tags: [User]
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        200:
+ *          description: 사용자 목록조회 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Users'
+ *        '400':
+ *          description: Bad Request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer
+ *                    example: 400
+ *                  message:
+ *                    type: string
+ *                    example: Bad Request
+ *        '401':
+ *          description: Unauthorized
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer
+ *                    example: 401
+ *                  message:
+ *                    type: string
+ *                    example: Unauthorized
+ */
 // 사용자 전체 조회 DESC 정렬
-router.get("/", async (req, res) => {
+router.get("/", token.verifyToken, user_controller.getAllUsers);
 
-  await Users.findAll({ order: [["id", "DESC"]] })
-    .then((result) => {
-      res
-        .status(200)
-        .json({ code: 200, message: "success", data: { users: result } });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+/**
+ * @swagger
+ * /user/{id}:
+ *    get:
+ *      summary: 사용자 조회
+ *      description: 사용자 조회
+ *      tags: [User]
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: integer
+ *          description: id
+ *      responses:
+ *        200:
+ *          description: 사용자 조회 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        '400':
+ *          description: Bad Request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer
+ *                    example: 400
+ *                  message:
+ *                    type: string
+ *                    example: Bad Request
+ *        '401':
+ *          description: Unauthorized
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer
+ *                    example: 401
+ *                  message:
+ *                    type: string
+ *                    example: Unauthorized
+ *        '404':
+ *          description: Not Found
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer
+ *                    example: 404
+ *                  message:
+ *                    type: string
+ *                    example: Not Found
+ */
 // get one user
-router.get("/:id", async (req, res) => {
-  await Users.findOne({
-    where: { id: req.params.id },
-  })
-    .then((result) => {
-      console.log(result);
-      if (result) {
-        res
-          .status(200)
-          .json({ code: 200, message: "success", data: { user: result } });
-      } else {
-        res.status(404).json({ code: 404, message: "not found" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/:id", token.verifyToken, user_controller.getUser);
 
 module.exports = router;

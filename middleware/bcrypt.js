771fe945dac56fd4ec2763a754c2b2c680157840
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-05-23 22:00:40 +0900

    Update bcrypt.js

--- middleware/bcrypt.js
+++ middleware/bcrypt.js
@@ -4,16 +4,11 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 
 // 암호화
-const encryptionPassword = (password) => {
+exports.encryptionPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
 // 복호화
-const decryptionPassword = (password, encryptionPwd) => {
+exports.decryptionPassword = (password, encryptionPwd) => {
   return bcrypt.compareSync(password, encryptionPwd);
-};
-
-module.exports = {
-  encryptionPassword,
-  decryptionPassword,
 };

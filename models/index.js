9db5b99c0ee7cd90d193e6d5c4829543a2993ac3
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-05-26 02:46:02 +0900

    Update index.js

--- models/index.js
+++ models/index.js
@@ -20,19 +20,38 @@ db.User = require("./user")(sequelize, Sequelize);
 db.Post = require("./post")(sequelize, Sequelize);
 db.Comment = require("./comment")(sequelize, Sequelize);
 
-
 /*모델 관계 정의*/
 
 // User Post
-db.User.hasMany(db.Post, { foreignKey: "user_id", sourceKey: "id", as:"post" });
-db.Post.belongsTo(db.User, { foreignKey: "user_id", targetKey: "id" ,as: "user" });
+db.User.hasMany(db.Post, {
+  foreignKey: "user_id",
+  sourceKey: "id",
+  as: "post",
+});
+db.Post.belongsTo(db.User, {
+  foreignKey: "user_id",
+  targetKey: "id",
+  as: "user",
+});
 
 // Post Comment
-db.Post.hasMany(db.Comment, { foreignKey: "post_id", sourceKey: "id", as: "comments" });
-db.Comment.belongsTo(db.Post, { foreignKey: "post_id", targetKey: "id", as:"post" });
+db.Post.hasMany(db.Comment, {
+  foreignKey: "post_id",
+  sourceKey: "id",
+  as: "comments",
+});
+db.Comment.belongsTo(db.Post, {
+  foreignKey: "post_id",
+  targetKey: "id",
+  as: "post",
+});
 
 // User Comment
 db.User.hasMany(db.Comment, { foreignKey: "user_id", sourceKey: "id" });
-db.Comment.belongsTo(db.User, { foreignKey: "user_id", targetKey: "id", as: "user" });
+db.Comment.belongsTo(db.User, {
+  foreignKey: "user_id",
+  targetKey: "id",
+  as: "user",
+});
 
 module.exports = db;
